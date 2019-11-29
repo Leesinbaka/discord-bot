357409bfa58b361f4e6462db24cff2c78f54f4da
Leesinbaka <kenneth72355700@gmail.com>
2019-11-30 01:27:14 +0800

    main function at the first day
    reading message and reponse
    reading message and send file

---
+++ PythonBot/discordbot.py
@@ -0,0 +1,22 @@
+import discord
+import random
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+        if message.content.startswith('hehe'):
+            i = random.randrange(1,14)
+            await message.channel.send(file=discord.File(str(i)+'.jpg'))
+client = MyClient()
+client.run('Mzg1MDAxNDY5NTI0NDQzMTM3.Xd_c8g.gyhBXG5V3LmIqjpsOPAJeLUQ7nk')
